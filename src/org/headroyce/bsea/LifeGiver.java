bb42192e07e9f0c228f3a909737bf6cd780c3347
cole.henrich <coleh2023@headroyce.org>
2021-02-05 14:07:54 -0800

    class LG and SW

---
+++ src/org/headroyce/bsea/LifeGiver.java
@@ -0,0 +1,12 @@
+package org.headroyce.bsea;
+
+import javafx.scene.paint.Color;
+
+public class LifeGiver extends Ball {
+    public LifeGiver(){
+        this(1);
+    }
+    public LifeGiver( double radius){
+        setColor(Color.GREEN);
+    }
+}
---
+++ src/org/headroyce/bsea/SpikedWall.java
@@ -0,0 +1,17 @@
+package org.headroyce.bsea;
+
+import javafx.scene.paint.Color;
+
+public class SpikedWall extends Obstacle{
+    public SpikedWall(){
+        this(10, 80, 0);
+    }
+    public SpikedWall(double width, double height, double x){
+        setColor(Color.AQUA);
+        setHeight(height);
+        setWidth(width);
+        if (x < 20 || x > 300) {
+            this.x = x;
+        }
+    }
+}
